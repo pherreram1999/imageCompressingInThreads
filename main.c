0b1c7895e4faa47d965cd654a4008a7ca36e26d4
sistemas <pherreram1600@alumno.ipn.mx>
2024-09-14 19:53:11 -0600

    Se ha implementado los chunks en imagenes

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include <sys/types.h>
 #include <sys/dir.h>
 #include <string.h>
+#include <dirent.h>
+#include <pthread.h>
 
 #define NUM_THREADS 4
 
@@ -12,6 +14,11 @@ typedef struct {
     int  len;
     char** paths;
 } ImagesResponse;
+
+typedef struct {
+    int len;
+    char** paths;
+} ImageTheadData;
 
 
 
@@ -72,44 +79,93 @@ void get_images(const char *path, ImagesResponse *response) {
 
 }
 
-void chunk_images(const ImagesResponse *images, const int chunkSize,const int numChunks,char ****chunks) {
+/*void chunk_images(const ImagesResponse *images, const int chunkSize,const int numChunks,char ****chunks) {
     int imageIndex = 0;
     for(int j = 0; j < numChunks; j++) {
         for(int i = 0; i < chunkSize; i++) {
-            if(imageIndex > images->len) return;
+            if(imageIndex >= images->len) {
+                // si no tenemos mas, liberamos los lotes restantes
+                free((*chunks)[j][i]);
+                continue;
+            }
             strcpy((*chunks)[j][i], images->paths[imageIndex]);
             imageIndex++;
         }
     }
+}*/
+
+void chunk_images(const ImagesResponse *response,const int chunk_size,const int chunk_num,ImagesResponse ***chunks) {
+    int index = 0;
+
+
+    for(int i = 0; i < chunk_num; i++) {
+        // cada response lo instancia en memoria
+        (*chunks)[i] = malloc(sizeof(ImagesResponse));
+        // instanciamos el arreglo de paths
+        (*chunks)[i]->paths = malloc(chunk_size * sizeof(char*));
+        (*chunks)[i]->len = 0;
+        for(int j = 0; j < chunk_size; j++) {
+            if(index >= response->len) {
+                // si ya no tenemos paths por copiar, limpiamos los lotes restantes
+                (*chunks)[i]->paths[j] = NULL;
+                continue;
+            }
+            // instancamos donde se va guardar la cadena
+            (*chunks)[i]->paths[j] = malloc(256 * sizeof(char));
+            strcpy((*chunks)[i]->paths[j],response->paths[index]);
+            (*chunks)[i]->len = (*chunks)[i]->len + 1;
+            index++;
+        }
+    }
+
+}
+
+void *image_processing(void * response) {
+    ImagesResponse *data = (ImagesResponse*)response;
+
 }
 
 
 int main(int argc, char **argv){
     ImagesResponse response;
-    get_images("/home/sistemas/Imágenes",&response);
+    get_images("/home/sistemas/Descargas",&response);
     // creamos un arreglo bidemesionasl para los chunks y dividirlos en hilos
-    int chunkSize = ceil((double) (response.len) / (double) (NUM_THREADS));
-    int numChunks = ceil((double) response.len / (double) chunkSize);
-    printf("NUM THREADS %d\n",NUM_THREADS);
-    printf("num chunks %d\n",numChunks);
-    printf("images len %d\n",response.len);
-    printf("chunkSize %d\n",chunkSize);
+    const int chunkSize = ceil((double) (response.len) / (double) (NUM_THREADS));
+    const int numChunks = ceil((double) response.len / (double) chunkSize);
 
-    char ***chunks = malloc(numChunks * sizeof(char**));
+    printf("* num threads:\t %d\n",NUM_THREADS);
+    printf("* num chunks:\t %d\n",numChunks);
+    printf("* images len:\t %d\n",response.len);
+    printf("* chunkSize:\t %d\n",chunkSize);
 
-    for(int k = 0; k < numChunks; k++) {
-        chunks[k] = malloc(chunkSize * sizeof(char*));
-        for(int i = 0; i < chunkSize; i++) {
-            chunks[k][i] = malloc(256 * sizeof(char));
-        }
-    }
-
+    // creamos el arreglo de responses
+    ImagesResponse **chunks = malloc(numChunks * sizeof(ImagesResponse*));
     chunk_images(&response,chunkSize,numChunks,&chunks);
 
-    for(int k = 0; k < numChunks; k++) {
-        for(int i = 0; i < chunkSize; i++) {
-            printf("string k => %d  i=>:%d | %s\n", k,i,chunks[k][i]);
+
+
+    /*int index = 0;
+    for(int i = 0; i < numChunks; i++) {
+        const ImagesResponse *r = chunks[i];
+
+        for(int j = 0; j < r->len; j++) {
+            printf("%d %s ",index,r->paths[j]);
+            index++;
         }
+    }
+    */
+
+    // creamos los hilos
+    pthread_t threads[numChunks];
+
+    for(int k = 0; k < numChunks; k++) {
+        pthread_create(&threads[k], NULL, image_processing, (void *)&chunks[k]);
+    }
+
+    // esperamos los threads
+
+    for(int k = 0; k < numChunks; k++) {
+        pthread_join(threads[k], NULL);
     }
 
     return 0;
